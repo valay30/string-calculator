7cc41bcd85c922464a18c14848627dc45a38a409
valay30 <patelvalay308@gmail.com>
2025-07-05 12:01:32 +0530

    Return number for a single number

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
 function add(numbers) {
-  if (numbers === "") return 0;
+  if (numbers === "") return 0; //empty string
+  return parseInt(numbers); // single input
 }
+
 module.exports = { add };
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,11 @@
 const { add } = require('./calculator');
 
+//for empty string
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
+});
+
+//for single input
+test('returns number for single input', () => {
+  expect(add("4")).toBe(4);
 });
